2d0e28d29ce166b3aae68e32dc4da3ef727184da
Samuel <70851632+samuellin01@users.noreply.github.com>
2026-04-23 12:54:37 -0700

    Add OSWorld eval results: 4c26e3f3-3a14-4d86-b44a-d3cedebbb487/baseline/trial_2

---
+++ osworld/multi_apps/4c26e3f3-3a14-4d86-b44a-d3cedebbb487/baseline/trial_2/step_0012/action.py
@@ -0,0 +1,2 @@
+import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('rm /home/user/Desktop/original_image.png\n'), shell=True, check=True)
+import time; time.sleep(0.05)
