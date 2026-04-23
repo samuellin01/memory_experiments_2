6fba7f01ba5331ff1b05fee20881b2e9cec441dd
Samuel <70851632+samuellin01@users.noreply.github.com>
2026-04-23 01:03:10 -0700

    Add OSWorld eval results: 1f18aa87-af6f-41ef-9853-cdb8f32ebdea/parallel/trial_2

--- osworld/multi_apps/20236825-b5df-46e7-89bf-62e1d640a897/baseline/trial_1/step_0005/action.py
+++
@@ -1,4 +0,0 @@
-import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('python3 ~/Desktop/bubbleSort.py > ~/Desktop/res.tx'), shell=True, check=True)
-import time; time.sleep(0.05)
-import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('t\n'), shell=True, check=True)
-import time; time.sleep(0.05)
