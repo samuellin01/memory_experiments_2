18bbf463df33c42de21f225ed18acbc80911a3e8
Samuel <70851632+samuellin01@users.noreply.github.com>
2026-04-23 09:22:44 -0700

    Add OSWorld eval results: 3680a5ee-6870-426a-a997-eba929a0d25c/parallel/trial_2

--- osworld/multi_apps/3a93cae4-ad3e-403e-8c12-65303b271818/baseline/trial_3/step_0013/action.py
+++
@@ -1,2 +0,0 @@
-import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('Lec 2 (12:00-14:00)'), shell=True, check=True)
-import time; time.sleep(0.05)
--- osworld/multi_apps/3a93cae4-ad3e-403e-8c12-65303b271818/baseline/trial_3/step_0022/action.py
+++
@@ -1,1 +0,0 @@
-pyautogui.click(658, 463)
