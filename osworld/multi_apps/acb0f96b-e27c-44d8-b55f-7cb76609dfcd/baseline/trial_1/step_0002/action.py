81ad41e4b8bd796e9ec5d92e7c646ae13a5976ef
Samuel <70851632+samuellin01@users.noreply.github.com>
2026-04-24 14:43:42 +0800

    Add OSWorld eval results: acb0f96b-e27c-44d8-b55f-7cb76609dfcd/baseline/trial_1

---
+++ osworld/multi_apps/acb0f96b-e27c-44d8-b55f-7cb76609dfcd/baseline/trial_1/step_0002/action.py
@@ -0,0 +1,4 @@
+import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('cd /home/user && git clone https://github.com/xlan'), shell=True, check=True)
+import time; time.sleep(0.05)
+import subprocess, shlex; subprocess.run(f'xdotool type --delay 12 -- ' + shlex.quote('g-ai/instructor-embedding.git\n'), shell=True, check=True)
+import time; time.sleep(0.05)
